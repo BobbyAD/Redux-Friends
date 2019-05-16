33e6c357f259ea30c86d50c2073ef64b2eb6f245
Robert Driskell <driskell.ba@gmail.com>
2019-05-16 17:54:54 -0500

    Added logout button

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import LoginForm from './components/LoginForm';
 import Friends from './components/Friends';
@@ -7,13 +7,20 @@ import PrivateRoute from './PrivateRoute';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Route exact path="/" component={LoginForm} />
-      <PrivateRoute exact path='/protected' component={Friends} />
-    </div>
-  );
+class App extends React.Component {
+    logout = () => {
+        localStorage.clear();
+        window.location.reload();
+    }
+    render() {
+        return (
+            <div className="App">
+                <button onClick={this.logout}>LOGOUT</button>
+                <Route exact path="/" component={LoginForm} />
+                <PrivateRoute exact path='/protected' component={Friends} />
+            </div>
+        );
+    }
 }
 
 export default App;
